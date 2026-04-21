266a82cdb3349d7b19b255415cfb8b67a012c33a
julia-shel <juliashelemyh@gmail.com>
2026-04-22 01:39:42 +0300

    Feature: App init: applying team comments

--- Lab1_Shelemykh/Program.cs
+++ Lab1_Shelemykh/Program.cs
@@ -3,6 +3,10 @@ using Lab1_Shelemykh.Repositories;
 using Lab1_Shelemykh.Services;
 using Lab1_Shelemykh;
 
+// This console application simulates a small bookstore management system.
+// It loads predefined books, customers, and orders into memory when the app starts.
+// The user can browse data, create new orders, and view simple sales reports.
+// Business logic is separated into services, while repositories handle in-memory data access.
 var repository = new InMemoryStoreRepository(
     SeedData.GetBooks(),
     SeedData.GetCustomers(),
